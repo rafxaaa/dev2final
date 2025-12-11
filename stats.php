bfda32f301ac0e31ad24f9f6ac195ca6340bd67d
ellenjung23 <ellenjun@usc.edu>
2025-12-10 22:17:46 -0800

    Update stats.php

--- stats.php
+++ stats.php
@@ -2,6 +2,14 @@
 <!doctype html>
 <html lang="en">
 <head>
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-F57J36XCNL"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-F57J36XCNL');
+</script>
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <title>SafeRoute — Analytics</title>
